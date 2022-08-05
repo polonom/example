8acedb400f51df791801580bb73a41b75e21255e
Polo_a <kiviku7@mail.ru>
2022-08-06 00:07:15 +0300

    Задача_1

---
+++ Seminar01_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB>numberA)
+{
+    Console.WriteLine($"max = {numberB}");
+}
+else {
+    Console.WriteLine($"max = {numberA} " );
+    }
